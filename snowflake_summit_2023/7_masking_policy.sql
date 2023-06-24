1a06cf22d4c0d4a39c8ac1157ce29f1719010ddf
veronikaSQL <52380877+veronikaSQL@users.noreply.github.com>
2023-06-24 19:57:05 -0400

    Update 7_masking_policy.sql

--- snowflake_summit_2023/7_masking_policy.sql
+++ snowflake_summit_2023/7_masking_policy.sql
@@ -9,20 +9,21 @@ use role sysadmin;
 use database raw;
 
  
---  tag for PII such as name and address. 
-create or replace tag raw.admin.pii_string;
+--  tag for BII such as name and address. 
 
--- masking policy. Only analyst_pii and sysadmin can see data in clear text
+create or replace tag raw.admin.bii_string;
+
+-- masking policy. Only analyst_bii and sysadmin can see data in clear text
 CREATE OR REPLACE MASKING POLICY RAW.ADMIN.NAME_MASKING AS (VAL STRING)
 returns string ->
 CASE 
-WHEN CURRENT_ROLE() ilike any ('ANALYST_PII','SYSADMIN') THEN VAL
+WHEN CURRENT_ROLE() ilike any ('ANALYST_BII','SYSADMIN') THEN VAL
     ELSE MD5(val)
 END;
 
 
 -- assign masking policy to TAG
-alter tag raw.admin.pii_string set masking policy RAW.ADMIN.NAME_MASKING;
+alter tag raw.admin.bii_string set masking policy RAW.ADMIN.NAME_MASKING;
 
 -- to alter masking policy, need to "unset" masking policy from tag, make changes and add it back. easy peasy 
 
